101047c0ef7929cb08901dbd88357280a19080bf
Richard.C <r.conde@live.com>
2021-10-20 08:37:52 -0400

    pasted chat.js example

--- js/app.js
+++ js/app.js
@@ -1,37 +1,76 @@
-
 let state = {
-
-    price: convertToNumber(document.querySelectorAll('[name="price"]')[0].value),
-    loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
-    down_payment: document.querySelectorAll('[name="down_payment"]')[0].value,
-    interest_rate: document.querySelectorAll('[name="interest_rate"]')[0].value,
-    property_tax: document.querySelectorAll('[name="property_tax"]')[0].value,
-    home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
-    hoa: document.querySelectorAll('[name="hoa"]')[0].value
+  price: convertToNumber(document.querySelectorAll('[name="price"]')[0].value),
+  loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
+  down_payment: document.querySelectorAll('[name="down_payment"]')[0].value,
+  interest_rate: document.querySelectorAll('[name="interest_rate"]')[0].value,
+  property_tax: document.querySelectorAll('[name="property_tax"]')[0].value,
+  home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
+  hoa: document.querySelectorAll('[name="hoa"]')[0].value,
 };
 
-let totalLoan, totalMonths, monthlyInterest, monthlyPrincipleInterest, monthlyPropertyTaxes,
-monthlyHomeInsurance, monthlyHOA,
-labels = ["Principle & Interest", "Property Tax", "Home Insurance", "HOA"],
-backgroundColor = [
+let totalLoan,
+  totalMonths,
+  monthlyInterest,
+  monthlyPrincipleInterest,
+  monthlyPropertyTaxes,
+  monthlyHomeInsurance,
+  monthlyHOA,
+  labels = ["Principle & Interest", "Property Tax", "Home Insurance", "HOA"],
+  backgroundColor = [
     "rgba(255, 99, 132,1)",
     "rgba(54,162,235,1)",
     "rgba(255, 206, 86,1)",
     "rgba(75, 192, 192, 1)",
     "rgba(153, 102, 255, 1)",
     "rgba(255, 159, 64, 1)",
-]
-borderColor =[
-    "rgba(255, 99, 132,1)",
-    "rgba(54,162,235,1)",
-    "rgba(255, 206, 86,1)",
-    "rgba(75, 192, 192, 1)",
-    "rgba(153, 102, 255, 1)",
-    "rgba(255, 159, 64, 1)",
-]
+  ];
+borderColor = [
+  "rgba(255, 99, 132,1)",
+  "rgba(54,162,235,1)",
+  "rgba(255, 206, 86,1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+];
 
-function convertToNumber(str){
-    return Number(str.replace(/[^0-9\.-]+/g, ""))
-};
+function convertToNumber(str) {
+  return Number(str.replace(/[^0-9\.-]+/g, ""));
+}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 console.log(state);
