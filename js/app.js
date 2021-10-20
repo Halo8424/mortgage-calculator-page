631bae24edb50de045a6d0f3b2fbf631d44d0565
Richard.C <r.conde@live.com>
2021-10-20 07:47:29 -0400

    name change

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
 let state = {
 
     price: document.querySelectorAll('[name="price"]')[0].value,
-    loanYears: document.querySelectorAll('[name="loan_years"]')[0].value,
-    downPayment: document.querySelectorAll('[name="down_payment"]')[0].value,
-    interestRate: document.querySelectorAll('[name="interest_rate"]')[0].value,
-    propertyTax: document.querySelectorAll('[name="property_tax"]')[0].value,
-    homeInsurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
+    loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
+    down_payment: document.querySelectorAll('[name="down_payment"]')[0].value,
+    interest_rate: document.querySelectorAll('[name="interest_rate"]')[0].value,
+    property_tax: document.querySelectorAll('[name="property_tax"]')[0].value,
+    home_insurance: document.querySelectorAll('[name="home_insurance"]')[0].value,
     hoa: document.querySelectorAll('[name="hoa"]')[0].value
 };
 
